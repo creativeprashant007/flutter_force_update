a132cebd2bc3d46995304e875d990ff4dec50203
Prashant Timalsina <prashanttimalsina@Prashants-MacBook-Air.local>
2023-06-29 22:10:25 +0545

    chore:main page refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart'
     show
-        AppBar,
         BuildContext,
-        Center,
         ColorScheme,
         Colors,
         MaterialApp,
-        Scaffold,
-        State,
-        StatefulWidget,
         StatelessWidget,
-        Text,
-        Theme,
         ThemeData,
         Widget,
         runApp;
-import 'package:upgrader/upgrader.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,34 +27,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Force Upgrade'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final cfg = AppcastConfiguration(
-      url:
-          "https://raw.githubusercontent.com/larryaasen/upgrader/master/test/testappcast.xml",
-      supportedOS: ['android']);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: UpgradeAlert(
-          upgrader: Upgrader(
-              dialogStyle: UpgradeDialogStyle.cupertino, appcastConfig: cfg),
-          child: Center(child: Text('Checking...')),
-        ));
   }
 }
